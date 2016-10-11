6312824ad82dd7f89dce210b2e8e980db94fc295
The Buildbot <buildbot@angular2buch.de>
2016-10-11 16:04:15 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/e3242b53ccaedf96434b451dfcaf58d33a17376a
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/166793712

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -41,11 +41,11 @@ export class BookFormComponent implements OnInit {
     this.myForm = this.fb.group({
       title: [book.title, Validators.required],
       subtitle: [book.subtitle],
-      isbn: [book.isbn, Validators.compose([
+      isbn: [book.isbn, [
         Validators.required,
         Validators.minLength(10),
         Validators.maxLength(13)
-      ])],
+      ]],
       description: [book.description],
       authors: this.buildAuthorsArray(book.authors),
       thumbnails: this.buildThumbnialsArray(book.thumbnails),
