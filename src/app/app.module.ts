419fa0a6da2f3c3c798dc458c857703c1a9e3928
The Buildbot <buildbot@angular2buch.de>
2017-11-27 12:01:37 +0000

    Auto-generated commit -- Update to new HttpClient
    - Import HttpClientModule from @angular/common/http (instead of HttpModule from @angular/http) in AppModule
    - Inject HttpClient from @angular/common/http (instead of Http) in the BookStoreService
    - remove mapping to response.json()
    - no JSON.stringify() needed anymore for POST/PUT requests
    - no Content-Type header needed anymore
    - responseType text for requests with non-JSON responses
    - generic type param for this.http.get when array of books is being retrieved (any[] will be replaced by BookRaw[] interface later)
    
    Signed-off-by: Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
    
    Triggered by commit: https://github.com/angular-buch/book-monkey2/commit/efeb008e9692ca1045f02ebe8070059d7678af31
    Travis build: https://travis-ci.org/angular-buch/book-monkey2/builds/307854537

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -27,7 +27,7 @@ import { DateValueAccessorModule } from 'angular-date-value-accessor';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     ReactiveFormsModule, // NEW
     AppRoutingModule,
     DateValueAccessorModule
